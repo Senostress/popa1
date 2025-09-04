19a76d9888c0ce177d2f58064bc7f23a86525fb5
Senostress <volodev1982@gmail.com>
2025-09-04 07:13:04 +0000

    commit2

---
+++ bookmark/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(0)
+
+  const handleAdd = () => { }
+
+  const handleSub = () => { }
+  return (
+    <>
+    <h1>{title}</h1>
+    <button> onClick={Change title</button>
+    </>
+  )
+}
+
+export default App
